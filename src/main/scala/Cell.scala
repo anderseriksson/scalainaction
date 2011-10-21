16813eddd590ab071edfed63a012b40f1f9e22a0
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-10-21 19:52:30 +0200

    Added implementation for method Cell.neighbours.

--- src/main/scala/Cell.scala
+++ src/main/scala/Cell.scala
@@ -1,4 +1,10 @@
 case class Cell(x: Int, y: Int) {
 
-  def neighbours: Set[Cell] = Set.empty
+  def neighbours: Set[Cell] = {
+    val neighbours = for {
+      xx <- x - 1 to x + 1
+      yy <- y - 1 to y + 1 if xx != x || yy != y
+    } yield Cell(xx, yy)
+    neighbours.toSet
+  }
 }
