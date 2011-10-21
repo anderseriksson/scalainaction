b4d2173f45dd77236a3b99783d299d395ee9b5de
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-10-21 20:10:26 +0200

    Added implementation for method Generation.next.

--- src/main/scala/Generation.scala
+++ src/main/scala/Generation.scala
@@ -1,3 +1,12 @@
 class Generation(val aliveCells: Set[Cell] = Set.empty) {
-  def next: Generation = new Generation()
+
+  def next: Generation = new Generation(stayingAlive ++ born)
+
+  private def stayingAlive = aliveCells filter (Set(2, 3) contains aliveNeighbours(_).size)
+
+  private def born = aliveCells flatMap deadNeighbours filter (aliveNeighbours(_).size == 3)
+
+  private def aliveNeighbours(cell: Cell) = cell.neighbours intersect aliveCells
+
+  private def deadNeighbours(cell: Cell) = cell.neighbours diff aliveCells
 }
