b9220ae1edee51d7938087fc569027d89b882c83
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-11-06 19:49:00 +0100

    Added operaton - to class Time.

--- src/main/scala/Time.scala
+++ src/main/scala/Time.scala
@@ -2,5 +2,7 @@ case class Time(hours: Int = 0, minutes: Int = 0) {
 
   val asMinutes = hours * 60 + minutes
 
+  def -(that: Time) = minus(that)
+
   def minus(that: Time) = this.asMinutes - that.asMinutes
 }
